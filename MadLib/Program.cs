f1d303e44c28b3108be360804e613ee675a23149
The Dad <Shihen1611@gmail.com>
2021-02-22 22:40:50 -0600

    fixing sign

--- MadLib/Program.cs
+++ MadLib/Program.cs
@@ -6,7 +6,31 @@ namespace MadLib
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Please fill in your answer");
+
+            Console.WriteLine("name a color.");
+            var color = Console.ReadLine();
+
+
+            Console.WriteLine($"favorite ethnic food.");
+            var food = Console.ReadLine();
+
+
+            Console.WriteLine($"a country");
+            var country = Console.ReadLine();
+
+            Console.WriteLine($"a verb");
+            var verb = Console.ReadLine();
+
+            Console.WriteLine($"a sport");
+            var sport = Console.ReadLine();
+
+            Console.WriteLine($"pizza topping");
+            var pizza = Console.ReadLine();
+
+            Console.WriteLine($"_-----------------------");
+
+            Console.WriteLine($"Once upon a time a young person who loved {color} computers thought that he would enroll in a TrueCoders course. His goal was to create a program called {food} that would help farmers who {verb} {sport} in {country} and raised {pizza}.");
         }
     }
 }
